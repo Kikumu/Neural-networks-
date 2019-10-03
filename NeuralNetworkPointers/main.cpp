6a8963eaa37279fc84a159382795b5ddf1d6066c
Ten5ei <scowty.sk@outlook.com>
2019-10-03 16:49:38 +0100

    ----

--- NeuralNetworkPointers/main.cpp
+++ NeuralNetworkPointers/main.cpp
@@ -6,7 +6,6 @@
 #include "opencv2/imgproc/imgproc.hpp"
 #include <opencv2/highgui/highgui.hpp>
 #include "outputLayer.h"
-#include "Matrix.h"
 #include "Training.h"
 #include "Eigen/Core"
 #include "LayerMaxPooling.h"
@@ -34,11 +33,15 @@ int main(int argc, char** argv) {
 	Mat img;
 	cvtColor(img_gray, img, cv::COLOR_BGR2GRAY);
 	double darray[100][100];
+
+	//INPUT
 	for (int i = 0; i != img_gray.cols; i++) {
 		for (int j = 0; j != img_gray.rows; j++) {
 			darray[i][j] = +(img_gray.at<char>(i, j)); //if you encounter an error during transfer of image data its probably here
 		}
 	}
+
+	//CONVOLUTIONS AND MAXPOOLING
 	conv.convole1(darray);
 	for(int i = 0; i < 3; i++)
 		maxpooling.poolConv(conv.featureMapData1[i]);
@@ -49,22 +52,26 @@ int main(int argc, char** argv) {
 	for (int i = 0; i < 12; i++)
 		conv.convolve3(maxpooling.pooledConv1[i]);
 
+	//FLATTENING
 	conv.flatten();
 	LayerFunc.forwardPropagate(conv.Flattened_features);
 	LayerFunc.forwardPropagate2(LayerFunc.firstLayerData);
 	LayerFunc.forwardPropagate3(LayerFunc.secondLayerData);
+
 	//probabilities
 	trn.softmaxVal_1 = LayerFunc.ThirdWeightData.at(0);
 	trn.softmaxVal_2 = LayerFunc.ThirdWeightData.at(1);
 	trn.funcSoftmax();
 	cst.network_output1.push_back(trn.output_data1);
 	cst.network_output1.push_back(trn.output_data2);
-   //LABEL
+
+    //LABEL
 	double label[2];
 	label[0] = 0.0;
 	label[1] = 1.0;
 	cst.actual_output1.push_back(label[0]);
 	cst.actual_output1.push_back(label[1]);
+
 	//PREDICTIONS
 	cout << "Predictions: ";
 	cout << "\n";
